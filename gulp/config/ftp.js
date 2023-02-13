b25344f77f760831b8da5551b41c64cdd8ba1f84
vnetti <98625446+vnetti@users.noreply.github.com>
2023-02-13 23:28:37 +0300

    Update ftp.js

--- gulp/config/ftp.js
+++ gulp/config/ftp.js
@@ -1,6 +1,6 @@
 export let configFTP = {
-    host: "141.8.192.151", // Адрес FTP сервера
-    user: "f0768447", // Имя пользователя
-    password: "amubuwigvi", // Пароль
+    host: "", // Адрес FTP сервера
+    user: "", // Имя пользователя
+    password: "", // Пароль
     parallel: 5 // Кол-во одновременных потоков
 }
